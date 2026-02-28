ad75fbacef9a3c84485000a519ea93a60d356667
Ghevary Pappo <ghevary@gmail.com>
2026-02-28 21:33:08 +0700

    feat: fix the admin docker auth

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -5,7 +5,7 @@ import * as schema from "./db/schema";
 
 export const auth = betterAuth({
     baseURL: process.env.BETTER_AUTH_URL || 'http://localhost:4000',
-    trustedOrigins: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    trustedOrigins: ['http://localhost:8080', 'http://localhost:5173', 'http://127.0.0.1:5173'],
     database: drizzleAdapter(db, {
         provider: "sqlite",
         schema: {
